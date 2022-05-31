0acaabac1e9940d8cc8c239c842c8d90e524dba4
Mauricio Figueroa <figueroa.a.mj@gmail.com>
2022-05-31 00:20:33 -0300

    readme

--- src/main/resources/db/migration/V2__create_test_users.sql
+++
@@ -1,8 +0,0 @@
-INSERT INTO users (version, creation_date, modification_date, app_user_role, enabled, locked, password,
-                   username)
-VALUES (1, '2022-05-29 14:59:20.000000', '2022-05-29 14:59:21.000000', 'USER', true, false,
-        '$2y$10$waCYzzdbkJY8KfBXTACgbenjPxETd0Amr.spFPdmolwxLGGlF41lm', 'user_1');
-INSERT INTO users (version, creation_date, modification_date, app_user_role, enabled, locked, password,
-                   username)
-VALUES (1, '2022-05-29 14:59:20', '2022-05-29 14:59:21', 'USER', true, false,
-        '$2y$10$waCYzzdbkJY8KfBXTACgbenjPxETd0Amr.spFPdmolwxLGGlF41lm', 'user_2');
